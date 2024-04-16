a8a5df8a2fa3e2cba4e794c129750d01777f6e35
danielaA01367611 <A01367611@tec.mx>
2024-04-15 19:22:23 -0600

    parte dos

--- ventas.py
+++ ventas.py
@@ -22,5 +22,23 @@ valores_nulos=df.isnull().sum()
 #quitar nulo en columna tarjetas_credito
 df['tarjetas_credito'] = df['tarjetas_credito'].fillna(round(df['tarjetas_credito'].median(),1))
 valores_nulos=df.isnull().sum()
+#print(valores_nulos)
+
+#print(df['otros_medios'].describe())
+
+df['otros_medios']=df['otros_medios'].fillna(6922148.759)
+valores_nulos=df.isnull().sum()
+#print(valores_nulos)
+
+df['subtotal_ventas_alimentos_bebidas'] =df['subtotal_ventas_alimentos_bebidas'].fillna(method='ffill')
+valores_nulos=df.isnull().sum()
+#print(valores_nulos)
+
+#rellena con valores del método bfill
+df['almacen'] =df['almacen'].fillna(method='bfill')
+valores_nulos=df.isnull().sum()
+#print(valores_nulos)
+
+df['panaderia'] =df['panaderia'].fillna(0)
+valores_nulos=df.isnull().sum()
 print(valores_nulos)
-     
