6ca77c5e54ad48acbacd6bb7addbe19a2432e112
danielaA01367611 <A01367611@tec.mx>
2024-04-18 23:44:31 -0600

    comentarios clientes

--- clientes.py
+++ clientes.py
@@ -5,11 +5,13 @@ df=pd.read_excel('clientes.xlsx')
 #print(df.head())
 #print(df.isnull().sum())
 
+#reemplacé este valor por un RFC generico, este dato se implanto en las columnas vacias
 df['RFC'].fillna('XAXX010101000', inplace=True)
 #print(df['RFC'])
 #print(df['RFC'].isnull().sum())
 #print(df.isnull().sum())
 
+#decidí poner el nombre que se mostraba en la columna de RFC porque no había más información del cliente
 df['NOMBRE'].fillna('MOSTR', inplace=True)
 valores_nulos=df.isnull().sum()
 #print(valores_nulos)
