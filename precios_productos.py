95961b870faa62876394b9316315cc58f9c6a303
danielaA01367611 <A01367611@tec.mx>
2024-04-18 23:46:44 -0600

    anotaciones precios productos

--- precios_productos.py
+++ precios_productos.py
@@ -5,6 +5,8 @@ df=pd.read_excel('precios_productos.xlsx')
 #print(df.head())
 #print(df.isnull().sum())
 
+#decidi imputar el nombre de Leticia Ramirez Hernandez porque er el anterior y el de después de los datos faltantes, por lo que lo mñas seguro es
+#que estos dos productos que tenian nulos fueran de la misma vendedora.
 df['NOMBRE_VENDEDOR']=df['NOMBRE_VENDEDOR'].fillna('LETICIA RAMIREZ HERNANDEZ')
 valores_nulos=df.isnull().sum()
 print(valores_nulos)
