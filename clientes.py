f18d9667f89a42c586edc5ad08fb3e1646e08a8d
danielaA01367611 <A01367611@tec.mx>
2024-04-18 20:33:31 -0600

    clientes limpio

--- clientes.py
+++ clientes.py
@@ -3,5 +3,15 @@ import numpy as np
 
 df=pd.read_excel('clientes.xlsx')
 #print(df.head())
-print(df.isnull().sum())
+#print(df.isnull().sum())
 
+df['RFC'].fillna('XAXX010101000', inplace=True)
+#print(df['RFC'])
+#print(df['RFC'].isnull().sum())
+#print(df.isnull().sum())
+
+df['NOMBRE'].fillna('MOSTR', inplace=True)
+valores_nulos=df.isnull().sum()
+#print(valores_nulos)
+
+df.to_csv('clientes_totales_limpio.csv')
